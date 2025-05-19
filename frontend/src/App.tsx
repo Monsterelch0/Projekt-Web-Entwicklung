69022d821493562816bf500c142fed79d7db8b0d
22Joshi22 <joshua.mueller2005@web.de>
2025-05-19 14:06:01 +0200

    Login Backend connection

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,7 @@ export default function App() {
   return (
     <Routes>
       
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<Login />} />
     </Routes>
   );
 }
