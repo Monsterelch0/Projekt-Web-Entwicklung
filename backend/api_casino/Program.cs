d876671a9048f8a410a5abfb897013eb5eb0b610
Monsterelch0 <leon@braches.info>
2025-05-21 00:02:00 +0200

    port nginx

--- backend/api_casino/Program.cs
+++ backend/api_casino/Program.cs
@@ -17,7 +17,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<UserService>();
-builder.WebHost.UseUrls("http://0.0.0.0:80");
+builder.WebHost.UseUrls("http://0.0.0.0:5296");
+
 
 
 
