062dcdf5f4ec1ebc68b3a675346e7a492b5a4386
Lea <lea@floofball.dev>
2025-05-24 17:59:55 +0200

    feat: backend prometheus metrics

--- backend/api_casino/Program.cs
+++ backend/api_casino/Program.cs
@@ -3,6 +3,7 @@ using CasinoApp.Interfaces;
 using CasinoApp.Repositories;
 using CasinoApp.Services;
 using Microsoft.EntityFrameworkCore;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,7 +12,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<IDeck, Deck>(); // Registriert Deck als Singleton für IDeck
+builder.Services.AddSingleton<IDeck, Deck>(); // Registriert Deck als Singleton fï¿½r IDeck
 builder.Services.AddSingleton<ICardFactory, CardFactory>(); // CardFactory kann auch Singleton oder Scoped/Transient sein
 
 
@@ -37,12 +38,12 @@ builder.Services.AddCors(options =>
         policy
             .WithOrigins(
                 "https://joshiidkwhy.de",  // Deine Produktions-URL
-                "http://localhost:5173",   // Für Vite Dev Server (Frontend)
-                "http://127.0.0.1:5173"  // Für Vite Dev Server (Frontend, alternative Zugriffsweise)
+                "http://localhost:5173",   // Fï¿½r Vite Dev Server (Frontend)
+                "http://127.0.0.1:5173"  // Fï¿½r Vite Dev Server (Frontend, alternative Zugriffsweise)
             )
             .AllowAnyHeader()  // Erlaubt alle HTTP-Header
             .AllowAnyMethod()  // Erlaubt alle HTTP-Methoden (GET, POST, PUT, etc.)
-            .AllowCredentials(); // Erlaubt das Senden von Credentials (z.B. Cookies, Authorization-Header). Vorsicht bei der Verwendung in Produktion mit nicht vertrauenswürdigen Origins.
+            .AllowCredentials(); // Erlaubt das Senden von Credentials (z.B. Cookies, Authorization-Header). Vorsicht bei der Verwendung in Produktion mit nicht vertrauenswï¿½rdigen Origins.
     });
 });
 // Listen on all IPs/ports inside Docker
@@ -58,8 +59,10 @@ app.UseSwaggerUI();
 // Middleware pipeline
 //app.UseHttpsRedirection(); // Optional, nur wenn du HTTPS in Docker aktiv hast
 app.UseRouting();
+app.UseHttpMetrics();
 app.UseCors("AllowFrontend");
 app.UseAuthorization();
+app.MapMetrics();
 app.Use(async (context, next) =>
 {
     Console.WriteLine($"Incoming request: {context.Request.Method} {context.Request.Path}");
